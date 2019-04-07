0db4a2050c7bf6b193c2c715efd9a0c5e3352bf7
winfordlin <46304148+winfordlin@users.noreply.github.com>
2019-04-07 17:10:42 -0400

    Add files via upload

---
+++ 28 Implement strStr().js
@@ -0,0 +1,54 @@
+/*
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+*/
+//Solution 1: O(N^2)
+
+const strStr = (haystack,needle) => {
+    if (needle === '') return 0;
+    let allclear = false;
+    for(let i = 0; i < haystack.length; i++){
+        for(let j = 0; j < needle.length; j++){
+            if (haystack[i+j] === needle[j]){
+                allclear = true;
+            }else{
+                allclear = false;
+                break;
+            }
+        } 
+        if (allclear){
+            return i;
+        } 
+    }
+    return -1;
+}
+
+//Solution 2: indexOf method O(N)
+
+const strStr2 = (haystack,needle) => {
+    return haystack.indexOf(needle);
+}
+
+//Solution 3: Substring method O(N)
+
+const strStr3 = (haystack,needle) => {
+
+    if (!needle) return 0;
+
+    for(let i = 0; i < haystack.length; i++){
+        if (haystack.substr(i,needle.length) === needle) return i;
+    }
+    
+    return -1;
+}
---
+++ 344 Reverse String.js
@@ -0,0 +1,27 @@
+/*
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+*/
+
+const reverseString = (charArray) => {
+    let l = 0, r = charArray.length-1;
+
+    while (l < r){
+        [charArray[l], charArray[r]] = [charArray[r], charArray[l]];
+        l++;
+        r--;
+    }
+    return charArray;
+}
