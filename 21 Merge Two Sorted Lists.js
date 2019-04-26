1b1c30a8954f42146d042e4695491bb7b9642043
winfordlin <46304148+winfordlin@users.noreply.github.com>
2019-04-26 14:23:07 -0400

    Add files via upload

--- 21 Merge Two Sorted Lists.js
+++ 21 Merge Two Sorted Lists.js
@@ -20,6 +20,28 @@ Output: 1->1->2->3->4->4
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
 var mergeTwoLists = function(l1, l2) {
-    
+    let l3 = new ListNode();
+    let cur = l3;
+
+    while(l1 || l2){
+        if (l1.val < l2.val){
+            cur.next = l1;
+            l1 = l1.next;
+        }else {
+            cur.next = l2;
+            l2 = l2.next;
+        }
+        cur = cur.next;
+    }
+    cur.next = l1 || l2;
+    return l3.next;
 };
+
+
