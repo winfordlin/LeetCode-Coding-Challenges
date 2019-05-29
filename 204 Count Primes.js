ffb436ac4c816acc136115f22bfe39f63339d36c
winfordlin <46304148+winfordlin@users.noreply.github.com>
2019-05-28 22:53:21 -0400

    Add files via upload

--- 204 Count Primes.js
+++ 204 Count Primes.js
@@ -1,7 +1,21 @@
+/*
+Count the number of prime numbers less than a non-negative number, n.
+
+Example:
+
+Input: 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+*/
 /**
  * @param {number} n
  * @return {number}
  */
 var countPrimes = function(n) {
-    
+    let isPrime = new Array(n).fill(false);
+    for(let i = 2; i < n; i++){
+        
+    }
 };
+
+console.log(countPrimes(5));
