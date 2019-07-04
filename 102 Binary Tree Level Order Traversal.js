441c1207719dbfef0d8bbcc2699200454da6be1a
winfordlin <46304148+winfordlin@users.noreply.github.com>
2019-07-04 12:13:38 -0400

    Add files via upload

--- 102 Binary Tree Level Order Traversal.js
+++ 102 Binary Tree Level Order Traversal.js
@@ -25,6 +25,34 @@ function TreeNode(val) {
  * @param {TreeNode} root
  * @return {number[][]}
  */
+
+
+var levelOrder = function(root) {
+    if (!root) {
+        return [];
+    }
+    const stack = [];
+    stack.push(root);
+    const result = [];
+    while (stack.length > 0) {
+        const size = stack.length;
+        const temp = [];
+        for (let i = 0; i < size; i++) {
+            const node = stack.shift();
+            temp.push(node.val);
+            if (node.left) {
+                stack.push(node.left);
+            }
+            if (node.right) {
+                stack.push(node.right);
+            }
+        }
+        result.push(temp);
+    }
+    return result;
+
+};
+ /*
 var levelOrder = function(root) {
     if (!root) {
         return null;
@@ -56,4 +84,4 @@ var levelOrder = function(root) {
         return container;
 };
 
-
+*/
