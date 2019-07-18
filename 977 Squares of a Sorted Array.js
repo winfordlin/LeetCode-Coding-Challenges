11ab5f2b69302f7b86326e27641a6192d829c0c8
winfordlin <46304148+winfordlin@users.noreply.github.com>
2019-07-18 15:26:11 -0400

    Add files via upload

---
+++ 977 Squares of a Sorted Array.js
@@ -0,0 +1,36 @@
+/*
+Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Example 2:
+
+Input: [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+
+*/
+
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortedSquares = function(A) {
+    let res = [];
+    let left = 0;
+    let right = A.length-1;
+
+    while(left <= right){
+        if (A[right] >= Math.abs(A[left])){
+            res.unshift(Math.pow(A[right],2));
+            right--;
+        } else{
+            res.unshift(Math.pow(A[left],2));
+            left++;
+        }
+    }
+    return res;
+};
