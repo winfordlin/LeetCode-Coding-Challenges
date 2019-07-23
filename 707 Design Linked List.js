a5bfc093f1eed4fc2304cf2f4ac63e5abf486657
winfordlin <46304148+winfordlin@users.noreply.github.com>
2019-07-23 01:23:07 -0400

    Add files via upload

---
+++ 707 Design Linked List.js
@@ -0,0 +1,124 @@
+/*
+Design your implementation of the linked list. You can choose to use the singly linked list or the doubly linked list. A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node. If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+
+Implement these functions in your linked list class:
+
+get(index) : Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+addAtHead(val) : Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+addAtTail(val) : Append a node of value val to the last element of the linked list.
+addAtIndex(index, val) : Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. If index is negative, the node will be inserted at the head of the list.
+deleteAtIndex(index) : Delete the index-th node in the linked list, if the index is valid.
+Example:
+
+MyLinkedList linkedList = new MyLinkedList();
+linkedList.addAtHead(1);
+linkedList.addAtTail(3);
+linkedList.addAtIndex(1, 2);  // linked list becomes 1->2->3
+linkedList.get(1);            // returns 2
+linkedList.deleteAtIndex(1);  // now the linked list is 1->3
+linkedList.get(1);            // returns 3
+Note:
+
+All values will be in the range of [1, 1000].
+The number of operations will be in the range of [1, 1000].
+Please do not use the built-in LinkedList library.
+*/
+
+/**
+ * Initialize your data structure here.
+ */
+
+function ListNode(val){
+    this.val = val;
+    this.next = null;
+}
+
+
+var MyLinkedList = function() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+};
+
+/**
+ * Get the value of the index-th node in the linked list. If the index is invalid, return -1. 
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function(index) {
+    let cur = this.head;
+    let i = 0;
+    while (cur && i <= index){
+        if (i === index) return cur.val;
+        cur = cur.next;
+        i = i + 1;
+    }
+    return -1;
+};
+
+/**
+ * Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function(val) {
+    let newHead = new ListNode(val);
+    if (!this.head){
+        this.tail = newHead;
+    }else{
+        newHead.next = this.head;
+    } 
+        this.head = newHead;
+        this.length = this.length + 1;
+};
+
+/**
+ * Append a node of value val to the last element of the linked list. 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function(val) {
+    let newNode = new ListNode(val);
+    let cur = this.head;
+    if (!cur){
+        this.head = newNode;
+    } else {
+        while(cur.next) cur = cur.next;
+        cur.next = newNode;  
+    }
+        this.tail = newNode;
+        this.length = this.length + 1;
+};
+
+/**
+ * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function(index, val) {
+    if (this.head){
+        if (!this.head.next && index === 0){
+            
+        }
+    }
+};
+
+/**
+ * Delete the index-th node in the linked list, if the index is valid. 
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function(index) {
+    
+};
+
+/** 
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
