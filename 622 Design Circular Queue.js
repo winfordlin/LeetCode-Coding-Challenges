c32322c1ac422f351803052773b258686a6018ef
winfordlin <46304148+winfordlin@users.noreply.github.com>
2019-07-23 20:19:25 -0400

    Add files via upload

--- 622 Design Circular Queue.js
+++ 622 Design Circular Queue.js
@@ -34,3 +34,45 @@ The number of operations will be in the range of [1, 1000].
 Please do not use the built-in Queue library.
 
 */
+
+class MyCircularQueue {
+    constructor(k) {
+      this.queue = Array(k);
+      this.k = k;
+      this.length = 0; // length of items having value
+      this.front = 0;
+      this.rear = 0;
+    }
+    enQueue(value) {
+      if (this.isFull()) {
+        return false;
+      }
+      this.queue[this.rear] = value;
+      this.rear = (this.rear + 1) % this.k;
+      this.length += 1;
+      return true;
+    }
+    deQueue() {
+      if (this.isEmpty()) {
+        return false;
+      }
+      delete this.queue[this.front];
+      this.front = (this.front + 1) % this.k;
+      this.length -= 1;
+      return true;
+    }
+    Front() {
+      return this.isEmpty() ? -1 : this.queue[this.front];
+    }
+    Rear() {
+      return this.isEmpty()
+        ? -1
+        : this.queue[this.rear === 0 ? this.k - 1 : this.rear - 1];
+    }
+    isEmpty() {
+      return this.length === 0;
+    }
+    isFull() {
+      return this.length === this.k;
+    }
+  }
