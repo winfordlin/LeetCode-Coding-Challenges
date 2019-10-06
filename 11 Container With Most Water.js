852de123dece5b537ff30db4df9d6179ac19e8dd
winfordlin <46304148+winfordlin@users.noreply.github.com>
2019-10-05 22:29:14 -0700

    Add files via upload

--- 11 Container With Most Water.js
+++ 11 Container With Most Water.js
@@ -11,20 +11,48 @@ Output: 49
  * @param {number[]} height
  * @return {number}
  */
+
+
+//Brute Force
+//Time: O(n^2) 
+//Space: O(1) 
+
+//Find all pairs of two lines, keep the rolling max
+
 var maxArea = function(height) {
+    let rollingMax = 0;
+    for(let i =0; i < height.length; i++){
+        for(let j = i + 1; j < height.length; j++){
+            let length = Math.min(height[i],height[j]);
+            let width = j - i;
+            rollingMax = Math.max(rollingMax,length*width);
+        }
+    }
+    return rollingMax;
+}
+
+
+//Two Pointer 
+//Time: O(n)
+//Space: O(1)
+
+//Initiate two pointers (max width), incrementally reduce the search space by the constraint of 
+//water can only be stored in the min of the two heights
+var maxArea2 = function(height){
+    let rollingMax = 0;
     let l = 0;
-    let r = height.length - 1;
-    let max = 0;
+    let r = height.length-1;
 
     while (l < r){
-        let total = Math.min(height[l],height[r]) * (r - l);
-        if (total > max) max = total;
-
-        if (height[l] <= height[r]){
+        let length = Math.min(height[l],height[r]);
+        let width = r - l;
+        rollingMax = Math.max(rollingMax,length*width);
+        if (height[l] < height[r]){
             l++;
         }else {
             r--;
         }
     }
-    return max;
-};
+    return rollingMax;
+}
+
