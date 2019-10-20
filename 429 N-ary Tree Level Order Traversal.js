26469c6ecf64a0616bd8395fd2c228936f79333e
winfordlin <46304148+winfordlin@users.noreply.github.com>
2019-10-19 21:20:41 -0700

    Add files via upload

---
+++ 429 N-ary Tree Level Order Traversal.js
@@ -0,0 +1,49 @@
+/*
+Given an n-ary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example, given a 3-ary tree:
+We should return its level order traversal:
+
+[
+     [1],
+     [3,2,4],
+     [5,6]
+]
+ 
+
+Note:
+
+The depth of the tree is at most 1000.
+The total number of nodes is at most 5000.
+*/
+
+function Node(val, children) {
+  this.val = val;
+  this.children = children;
+}
+
+/*
+ * @param {Node} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  if (!root) return [];
+
+  let queue = [];
+  let output = [];
+  queue.push(root);
+
+  while (queue.length) {
+    let level = [];
+    let size = queue.length;
+    for (let i = 0; i < size; i++) {
+      let temp = queue.shift();
+      level.push(temp.val);
+      for (let i = 0; i < temp.children.length; i++) {
+        queue.push(temp.children[i]);
+      }
+    }
+    output.push(level);
+  }
+  return output;
+};
