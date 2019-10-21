e3e0283eb75fab38437b60475903fea0b2777d22
winfordlin <46304148+winfordlin@users.noreply.github.com>
2019-10-21 15:54:19 -0700

    Add files via upload

---
+++ 230 Kth Smallest Element in a BST.js
@@ -0,0 +1,58 @@
+/* 
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Example 1:
+
+Input: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+Output: 1
+Example 2:
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+Output: 3
+Follow up:
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+*/
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+  if (!root) return null;
+
+  let stack = [];
+  let output = [];
+
+  while (stack.length || root) {
+    while (root) {
+      stack.push(root);
+      root = root.left;
+    }
+    root = stack.pop();
+    output.push(root.val);
+    root = root.right;
+  }
+
+  return output[k - 1];
+};
