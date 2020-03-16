20c21a7c0da141a0ac3c5c5a00d4475647cf51d9
winfordlin <46304148+winfordlin@users.noreply.github.com>
2020-03-16 18:04:09 -0400

    Add files via upload

---
+++ 451 Sort Characters By Frequency.js
@@ -0,0 +1,66 @@
+/*
+Given a string, sort it in decreasing order based on the frequency of characters.
+
+Example 1:
+
+Input:
+"tree"
+
+Output:
+"eert"
+
+Explanation:
+'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+Example 2:
+
+Input:
+"cccaaa"
+
+Output:
+"cccaaa"
+
+Explanation:
+Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+Note that "cacaca" is incorrect, as the same characters must be together.
+Example 3:
+
+Input:
+"Aabb"
+
+Output:
+"bbAa"
+
+Explanation:
+"bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function (s) {
+    let freq = {};
+    let arr = [];
+    let res = [];
+
+    for (let i = 0; i < s.length; i++) {
+        freq[s[i]] = ++freq[s[i]] || 1;
+    }
+
+    for (let key in freq) {
+        arr.push([key, freq[key]]);
+    }
+
+    arr.sort((a, b) => b[1] - a[1]);
+
+    arr.forEach((num) => {
+        for (let i = 0; i < num[1]; i++) {
+            res.push(num[0]);
+        }
+    });
+
+    return res.join("");
+
+};
