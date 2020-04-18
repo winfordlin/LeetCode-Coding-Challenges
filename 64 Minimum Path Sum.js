41c1205e4740388084be0a3f9db18cfb06fe17a4
winfordlin <46304148+winfordlin@users.noreply.github.com>
2020-04-18 17:15:48 -0400

    Add files via upload

---
+++ 64 Minimum Path Sum.js
@@ -0,0 +1,44 @@
+/*
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+Example:
+
+Input:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+Output: 7
+Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+*/
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+    
+    let m = grid.length;
+    let n = grid[0].length;
+
+    //update first row
+    for(let i = 1; i < m; i++){
+        grid[0][i] += grid[0][i-1];
+    }
+
+    //update first column
+    for(let i = 1; i < n; i++){
+        grid[i][0] += grid[i-1][0];
+    }
+
+    for(let i = 1; i < m; i++){
+        for(let j = 1; j < n; j++){
+            grid[i][j] += Math.min(grid[i-1][j],grid[i][j-1]);
+        }
+    }
+    
+    return grid[m-1][n-1];
+};
