b062a05fa17fb3a4013f20383b670bbcc3b87074
winfordlin <46304148+winfordlin@users.noreply.github.com>
2020-08-26 10:06:07 -0400

    Add files via upload

---
+++ 1396 Design Underground System.js
@@ -0,0 +1,75 @@
+/*
+Implement the class UndergroundSystem that supports three methods:
+
+1. checkIn(int id, string stationName, int t)
+
+A customer with id card equal to id, gets in the station stationName at time t.
+A customer can only be checked into one place at a time.
+2. checkOut(int id, string stationName, int t)
+
+A customer with id card equal to id, gets out from the station stationName at time t.
+3. getAverageTime(string startStation, string endStation) 
+
+Returns the average time to travel between the startStation and the endStation.
+The average time is computed from all the previous traveling from startStation to endStation that happened directly.
+Call to getAverageTime is always valid.
+You can assume all calls to checkIn and checkOut methods are consistent. That is, if a customer gets in at time t1 at some station, then it gets out at time t2 with t2 > t1. All events happen in chronological order.
+*/
+
+var UndergroundSystem = function () {
+  this.checkingIn = {};
+  this.checkOutTime = {};
+};
+
+/**
+ * @param {number} id
+ * @param {string} stationName
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkIn = function (id, stationName, t) {
+  this.checkingIn[id] = [stationName, t];
+};
+
+/**
+ * @param {number} id
+ * @param {string} stationName
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkOut = function (id, stationName, t) {
+  let key = `${this.checkingIn[id][0]},${stationName}`;
+  let avg = t - this.checkingIn[id][1];
+
+  if (!this.checkOutTime[key]) {
+    this.checkOutTime[key] = [avg, 1];
+  } else {
+    this.checkOutTime[key][0] += avg;
+    this.checkOutTime[key][1]++;
+  }
+};
+
+/**
+ * @param {string} startStation
+ * @param {string} endStation
+ * @return {number}
+ */
+UndergroundSystem.prototype.getAverageTime = function (
+  startStation,
+  endStation
+) {
+  let key = `${startStation},${endStation}`;
+  if (!this.checkOutTime[key]) {
+    return null;
+  } else {
+    return this.checkOutTime[key][0] / this.checkOutTime[key][1];
+  }
+};
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * var obj = new UndergroundSystem()
+ * obj.checkIn(id,stationName,t)
+ * obj.checkOut(id,stationName,t)
+ * var param_3 = obj.getAverageTime(startStation,endStation)
+ */
